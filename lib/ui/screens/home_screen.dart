45c807658d51a2d14b2aaf0d5a7bc6a90d0380b0
Kshitij-25 <kshitijnishu@gmail.com>
2020-04-16 15:37:09 +0530

    vertical categories

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -7,6 +7,7 @@ import 'package:netflix_redesign/ui/widgets/homepagefeaturedwidget.dart';
 import 'package:netflix_redesign/ui/widgets/movie_container.dart';
 import 'package:netflix_redesign/ui/widgets/section_container.dart';
 import 'package:netflix_redesign/ui/widgets/tv_container.dart';
+import 'package:netflix_redesign/ui/widgets/vertical_categories.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -33,152 +34,157 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       // backgroundColor: Colors.white,
-      appBar: AppBar(
-        // backgroundColor: Colors.white,
-        elevation: 0,
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.search,
-              color: Colors.white,
-            ),
-            onPressed: () {},
-          ),
-        ],
-        leading: IconButton(
-          icon: Icon(
-            Icons.menu,
-            color: Colors.white,
-          ),
-          onPressed: () {},
-        ),
-        title: Text(
-          'ChillFlix',
-          style: TextStyle(
-            color: Colors.red,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
+      // appBar: AppBar(
+      //   // backgroundColor: Colors.white,
+      //   elevation: 0,
+      //   actions: <Widget>[
+      //     IconButton(
+      //       icon: Icon(
+      //         Icons.search,
+      //         color: Colors.white,
+      //       ),
+      //       onPressed: () {},
+      //     ),
+      //   ],
+      //   leading: IconButton(
+      //     icon: Icon(
+      //       Icons.menu,
+      //       color: Colors.white,
+      //     ),
+      //     onPressed: () {},
+      //   ),
+      //   title: Text(
+      //     'ChillFlix',
+      //     style: TextStyle(
+      //       color: Colors.red,
+      //       fontWeight: FontWeight.bold,
+      //     ),
+      //   ),
+      //   centerTitle: true,
+      // ),
+      body: SafeArea(
+        child: Row(
           children: <Widget>[
-            Container(
-              height: MediaQuery.of(context).size.height / 3,
-              child: FutureBuilder<List<FeaturedMovieModel>>(
-                future: featuredMovies,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return HomePageFeaturedWidget(snapshot: snapshot);
-                  } else {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                },
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.only(left: 5.0),
-              height: 61,
-              child: FutureBuilder<List<GenreModel>>(
-                future: genreList,
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return ListView.builder(
-                      itemCount: snapshot.data.length,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (ctx, id) {
-                        return Container(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15.0),
-                            color: Colors.red,
-                            boxShadow: [
-                              BoxShadow(
-                                color: Colors.red,
-                                blurRadius: 2.5,
-                              )
-                            ],
-                          ),
-                          // width: MediaQuery.of(context).size.width / 2.5,
-                          constraints: BoxConstraints(minWidth: 150),
-                          alignment: Alignment.center,
-                          margin: EdgeInsets.symmetric(
-                            horizontal: 7,
-                            vertical: 5.0,
-                          ),
-                          padding:
-                              const EdgeInsets.symmetric(horizontal: 5.0),
-                          child: Text(
-                            "${snapshot.data[id].name}",
-                            style: Theme.of(context)
-                                .textTheme
-                                .headline
-                                .apply(color: Colors.white),
-                          ),
-                        );
-                      },
-                    );
-                  } else {
-                    return Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                },
-              ),
-            ),
             SizedBox(
-              height: 15,
-            ),
-            SectionContainer(
-              sectionTitle: "My List",
+              width: 45,
               child: Container(
-                height: MediaQuery.of(context).size.height / 3,
-                child: FutureBuilder<List<FeaturedTvModel>>(
-                  future: featuredTv,
-                  builder: (ctx, snapshot) {
-                    if (snapshot.hasData) {
-                      return ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (ctx, id) {
-                          return TvContainer(snapshot: snapshot.data[id]);
-                        },
-                      );
-                    } else {
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                  },
+                padding: const EdgeInsets.symmetric(vertical: 9),
+                height: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(25),
+                    bottomRight: Radius.circular(25),
+                  ),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.home),
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.search),
+                      onPressed: () {},
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 11.0),
+                        child: VerticalCategories(),
+                      ),
+                    )
+                  ],
                 ),
               ),
             ),
-            SizedBox(
-              height: 15,
-            ),
-            SectionContainer(
-              sectionTitle: "Popular on ChillFlix",
-              child: Container(
-                height: MediaQuery.of(context).size.height / 3,
-                child: FutureBuilder<List<FeaturedMovieModel>>(
-                  future: featuredMovies,
-                  builder: (ctx, snapshot) {
-                    if (snapshot.hasData) {
-                      return ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (ctx, id) {
-                          return MovieContainer(snapshot: snapshot.data[id]);
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15),
+                      child: Text(
+                        'ChillFlix',
+                        style: TextStyle(
+                          fontSize: 27,
+                          color: Colors.red,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      height: MediaQuery.of(context).size.height / 3,
+                      child: FutureBuilder<List<FeaturedMovieModel>>(
+                        future: featuredMovies,
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            return HomePageFeaturedWidget(snapshot: snapshot);
+                          } else {
+                            return Center(
+                              child: CircularProgressIndicator(),
+                            );
+                          }
                         },
-                      );
-                    } else {
-                      return Center(
-                        child: CircularProgressIndicator(),
-                      );
-                    }
-                  },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    SectionContainer(
+                      sectionTitle: "My List",
+                      child: Container(
+                        height: MediaQuery.of(context).size.height / 3,
+                        child: FutureBuilder<List<FeaturedTvModel>>(
+                          future: featuredTv,
+                          builder: (ctx, snapshot) {
+                            if (snapshot.hasData) {
+                              return ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: snapshot.data.length,
+                                itemBuilder: (ctx, id) {
+                                  return TvContainer(
+                                      snapshot: snapshot.data[id]);
+                                },
+                              );
+                            } else {
+                              return Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    SectionContainer(
+                      sectionTitle: "Popular on ChillFlix",
+                      child: Container(
+                        height: MediaQuery.of(context).size.height / 3,
+                        child: FutureBuilder<List<FeaturedMovieModel>>(
+                          future: featuredMovies,
+                          builder: (ctx, snapshot) {
+                            if (snapshot.hasData) {
+                              return ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: snapshot.data.length,
+                                itemBuilder: (ctx, id) {
+                                  return MovieContainer(
+                                      snapshot: snapshot.data[id]);
+                                },
+                              );
+                            } else {
+                              return Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
--- lib/ui/screens/tv_details_screen.dart
+++ lib/ui/screens/tv_details_screen.dart
@@ -30,127 +30,125 @@ class _TvDetailsScreenState extends State<TvDetailsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        // backgroundColor: Colors.white,
-        body: FutureBuilder<TvModel>(
-          future: tvModel,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    MyHero(
-                      imgUrl: getPosterImage(snapshot.data.poster_path),
-                    ),
-                    SizedBox(
-                      height: 11,
-                    ),
-                    Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 15),
-                      child: Column(
-                        children: <Widget>[
-                          Text(
-                            '${snapshot.data.original_name}',
-                            style: Theme.of(context).textTheme.headline,
-                          ),
-                          SizedBox(
-                            height: 7,
-                          ),
-                          RichText(
-                            text: TextSpan(
-                              children: List.generate(
-                                snapshot.data.genre.length,
-                                (i) {
-                                  return TextSpan(
-                                      text:
-                                          '${snapshot.data.genre[i]['name']}');
-                                },
-                              ),
-                              style: Theme.of(context).textTheme.caption,
+    return Scaffold(
+      // backgroundColor: Colors.white,
+      body: FutureBuilder<TvModel>(
+        future: tvModel,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  MyHero(
+                    imgUrl: getPosterImage(snapshot.data.poster_path),
+                  ),
+                  SizedBox(
+                    height: 11,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          '${snapshot.data.original_name}',
+                          style: Theme.of(context).textTheme.headline,
+                        ),
+                        SizedBox(
+                          height: 7,
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            children: List.generate(
+                              snapshot.data.genre.length,
+                              (i) {
+                                return TextSpan(
+                                    text:
+                                        '${snapshot.data.genre[i]['name']}');
+                              },
                             ),
+                            style: Theme.of(context).textTheme.caption,
                           ),
-                          SizedBox(
-                            height: 9,
+                        ),
+                        SizedBox(
+                          height: 9,
+                        ),
+                        RatingBar(
+                          initialRating: snapshot.data.rating,
+                          direction: Axis.horizontal,
+                          allowHalfRating: true,
+                          itemCount: 10,
+                          itemPadding:
+                              const EdgeInsets.symmetric(horizontal: 4),
+                          itemBuilder: (context, _) => Icon(
+                            Icons.star,
+                            color: Colors.amber,
                           ),
-                          RatingBar(
-                            initialRating: snapshot.data.rating,
-                            direction: Axis.horizontal,
-                            allowHalfRating: true,
-                            itemCount: 10,
-                            itemPadding:
-                                const EdgeInsets.symmetric(horizontal: 4),
-                            itemBuilder: (context, _) => Icon(
-                              Icons.star,
-                              color: Colors.amber,
+                          itemSize: 25,
+                          onRatingUpdate: (rating) {
+                            print(rating);
+                          },
+                        ),
+                        SizedBox(
+                          height: 13,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            Column(
+                              children: <Widget>[
+                                Text(
+                                  'Year',
+                                  style: Theme.of(context).textTheme.caption,
+                                ),
+                                Text(
+                                  '${DateTime.parse(snapshot.data.first_air_date).year}',
+                                  style: Theme.of(context).textTheme.subhead,
+                                ),
+                              ],
                             ),
-                            itemSize: 25,
-                            onRatingUpdate: (rating) {
-                              print(rating);
-                            },
-                          ),
-                          SizedBox(
-                            height: 13,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: <Widget>[
-                              Column(
-                                children: <Widget>[
-                                  Text(
-                                    'Year',
-                                    style: Theme.of(context).textTheme.caption,
-                                  ),
-                                  Text(
-                                    '${DateTime.parse(snapshot.data.first_air_date).year}',
-                                    style: Theme.of(context).textTheme.subhead,
-                                  ),
-                                ],
-                              ),
-                              Column(
-                                children: <Widget>[
-                                  Text(
-                                    'Country',
-                                    style: Theme.of(context).textTheme.caption,
-                                  ),
-                                  Text(
-                                    '${snapshot.data.origin_country}',
-                                    style: Theme.of(context).textTheme.subhead,
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 13,
-                          ),
-                          Text(
-                            '${snapshot.data.overview}',
-                            textAlign: TextAlign.center,
-                            style: Theme.of(context)
-                                .textTheme
-                                .body1
-                                .apply(fontSizeFactor: 1.2),
-                          ),
-                          SizedBox(
-                            height: 13,
-                          ),
-                        ],
-                      ),
+                            Column(
+                              children: <Widget>[
+                                Text(
+                                  'Country',
+                                  style: Theme.of(context).textTheme.caption,
+                                ),
+                                Text(
+                                  '${snapshot.data.origin_country}',
+                                  style: Theme.of(context).textTheme.subhead,
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 13,
+                        ),
+                        Text(
+                          '${snapshot.data.overview}',
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context)
+                              .textTheme
+                              .body1
+                              .apply(fontSizeFactor: 1.2),
+                        ),
+                        SizedBox(
+                          height: 13,
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      height: 13,
-                    ),
-                  ],
-                ),
-              );
-            } else {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
-        ),
+                  ),
+                  SizedBox(
+                    height: 13,
+                  ),
+                ],
+              ),
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
